5654882dfb70d0ae01999a6d0e5342172d5d2aea
yangqun <yangqun.amy@gmail.com>
2020-06-28 22:39:27 +0800

    update

---
+++ src/views/register/Register.tsx
@@ -0,0 +1,8 @@
+import { Component as VueComponent } from 'vue-tsx-support'
+import { Component } from 'vue-property-decorator'
+@Component
+export default class Register extends VueComponent<{}> {
+  render() {
+    return <div>register</div>
+  }
+}
