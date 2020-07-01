7ccaccb8cd2fa774df07a5e343e18f3913db7035
yangqun <yangqun.amy@gmail.com>
2020-07-02 00:39:54 +0800

    change package

--- src/lib/error-handler.ts
+++ src/lib/error-handler.ts
@@ -11,18 +11,20 @@ declare module 'vue/types/vue' {
 
 const serverError = 'Network Error, please try again later.'
 
-const errorHandler = { install(Vue: typeof _Vue, options?: any) {
-  Vue.handleError = (error: { code: string; message: string; response: any }) => {
-    // console.error(error)
-    if (error.response) {
-      console.log('error.response')
-      window.vue.$toast.fail(serverError)
-    } else {
-      console.log('normal error')
-      window.vue.$toast.fail(error.message)
+const errorHandler = {
+  install(Vue: typeof _Vue, options?: any) {
+    Vue.handleError = (error: { code: string; message: string; response: any }) => {
+      // console.error(error)
+      if (error.response) {
+        console.log('error.response')
+        window.vue.$toast.fail(serverError)
+      } else {
+        console.log('normal error')
+        window.vue.$toast.fail(error.message)
+      }
     }
-  }
-  Vue.prototype.$handleError = Vue.handleError
-} }
+    Vue.prototype.$handleError = Vue.handleError
+  },
+}
 
 export default errorHandler
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-
 import VanUi from '@/lib/van-ui'
 import errorHandler from '@/lib/error-handler'
 
